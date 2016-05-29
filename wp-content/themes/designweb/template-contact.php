3327a7d4250eb6e3e08e453a004f07e6ee4c596b
Sean H <sean@designweblouisville.com>
2016-05-29 15:13:31 -0400

    Updating basic theme

---
+++ wp-content/themes/designweb/template-contact.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Template name: Contact
+*/
+get_header(); ?>
+
+	<section class="contact">
+
+	</section>
+	</main><!-- #main -->
+	</div><!-- #primary -->
+	<?php get_footer(); ?>
---
+++ wp-content/themes/designweb/template-home.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Template name: Homepage
+*/
+get_header(); ?>
+
+	<section class="homepage">
+
+	</section>
+	</main><!-- #main -->
+	</div><!-- #primary -->
+	<?php get_footer(); ?>
